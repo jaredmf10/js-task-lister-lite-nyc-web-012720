4a26fc53549ea84aaeca6cdbe481b871bc64a466
Jared Fleming <jaredmf10@gmail.com>
2020-03-11 08:53:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
+const form = document.getElementById('create-task-form')
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //find target (the button)
+  //add event listener (click) to target (the button)
+  //create post request on click
+  // add to tasks somehow
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    // console.log(event.target["new-task-description"].value)
+    var taskDescription = document.getElementById("new-task-description").value
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(taskDescription);
+    node.appendChild(textnode);
+    document.getElementById("tasks").appendChild(node);
+  })
 });
